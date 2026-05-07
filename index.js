f2f23f02f4b382f9b538e68737ba74b42f827e8f
shakiburrahmanpratogit config --global user.email <shakiburrahmanpranto@gmail.com>
2026-05-07 18:53:37 +0600

    debug firebase env

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const app = express();
 const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 
-const serviceAccount = require("./smart-deals-firebase-admin.json");
+//const serviceAccount = require("./smart-deals-firebase-admin.json");
+
+// index.js
+console.log("FIREBASE KEY:", process.env.FIREBASE_SERVICE_KEY);
+
+const decoded = Buffer.from(
+  process.env.FIREBASE_SERVICE_KEY,
+  "base64",
+).toString("utf8");
+
+const serviceAccount = JSON.parse(decoded);
+const decoded = Buffer.from(
+  process.env.FIREBASE_SERVICE_KEY,
+  "base64",
+).toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -310,7 +325,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
